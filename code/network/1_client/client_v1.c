4c62b35d42ee22ca6984993c1e02b0ccc9d0305f
htwoo111 <1405360652@qq.com>
2021-02-03 01:20:00 +0800

    update server_v2

--- code/network/1_client/client_v1.c
+++ code/network/1_client/client_v1.c
@@ -8,8 +8,8 @@
 
 void run() {
   // initalization
-  printf("startting...\n");
-  int cfd, sfd, nwrite, nread, nreadStdin;
+  printf("(client)startting...\n");
+  int cfd, sfd, nwrite, nread;
   struct sockaddr_in saddr;
   // char *msg = "hello, i am client";
   char buf[BUFSIZ];
@@ -19,27 +19,31 @@ void run() {
   cfd = Socket_2();
   Bind_2(cfd, CLIENT_ADDR, CLIENT_PORT);
   sfd = Connect_2(cfd, &saddr);
+  printf("(client)server fd = %d\n", sfd);
 
   if (sfd < 0) {
     fprintf(stderr, "connect server fail\n");
     exit(EXIT_FAILURE);
   }
-  printf("connect server successfully!\n");
+  printf("(client)connect server successfully!\n");
   while (fgets(buf, BUFSIZ, stdin)) {
     nwrite = write(sfd, buf, strlen(buf));
     if (nwrite <= 0) {
       fprintf(stderr, "(client)write fail\n");
       exit(EXIT_FAILURE);
     }
+    bzero(buf, BUFSIZ);
     nread = read(sfd, buf, BUFSIZ);
     if (nread <= 0) {
       fprintf(stderr, "(client)read fail\n");
       exit(EXIT_FAILURE);
     }
+    buf[nread] = '\0';
     printf("(client)read message from server:%s\n", buf);
   }
   Close(sfd);
   Close(cfd);
+  printf("(client)ending...\n");
 }
 
 int main() {
--- code/network/1_server/server_v2.c
+++ code/network/1_server/server_v2.c
@@ -19,15 +19,19 @@ int run() {
 
   // implementation
   sfd = Socket_2();
+  fprintf(stdout, "(server)startting\n");
   Bind_2(sfd, SERVER_ADDR, SERVER_PORT);
   Listen_2(sfd);
   cfd = Accept_2(sfd, &caddr, &slen);
   if (cfd < 0) {
+    fprintf(stderr, "(server)accept client fail\n");
     exit(EXIT_FAILURE);
   }
 
   // show information about client
+  printf("(server)ntop...\n");
   inet_ntop(cfd, &caddr.sin_addr, clientIpBuf, sizeof(struct sockaddr_in));
+  printf("(server)ntohl....\n");
   cport = ntohl(caddr.sin_port);
   printf("accept client: ip=%s, port = %d\n", clientIpBuf, cport);
 
@@ -37,20 +41,22 @@ int run() {
       fprintf(stderr, "read message from client fail!");
       exit(EXIT_FAILURE);
     }
+    buf[nread] = '\0';
     printf("(server): read message from client:%s\n", buf);
 
-    int len = strlen(buf);
     char c;
-    for (int i = 0; i < len; ++i) {
+    for (int i = 0; i < nread; ++i) {
       c = buf[i];
       buf[i] = toupper(c);
     }
-    nwrite = write(cfd, buf, len + 1);
+    nwrite = write(cfd, buf, nread + 1);
     if (nwrite <= 0) {
       fprintf(stderr, "fail to send message to client");
       exit(EXIT_FAILURE);
     }
   }
+  Close_2(sfd, true);
+  printf("(server)ending\n");
   return 0;
 }
 
